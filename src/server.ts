b1a57bbb9bcf64c4738f109e5dcc632187945c04
romluc <romulofilhofilho@gmail.com>
2020-05-02 11:22:24 -0300

    Setup editorconfig

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = 3333;
 
-app.get('/', (request, response) => {
-  return response.send('Hello, maaaaaannnns!');
+app.get("/", (request, response) => {
+  return response.send("Hello, maaaaaannnns!");
 });
 
 app.listen(PORT, () => {
