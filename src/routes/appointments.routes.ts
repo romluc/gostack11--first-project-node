72173369b527e84777b539758eb2ad82b35976e1
romluc <romulofilhofilho@gmail.com>
2020-05-04 22:39:37 -0300

    Add date-fns

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO } from 'date-fns';
 
 const appointmentsRouter = Router();
 
+const appointments = [];
+
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({ message: 'Hello maaans!' });
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
